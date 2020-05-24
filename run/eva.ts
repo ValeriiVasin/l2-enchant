7df9553e7cb0dd62887e21a7d4e9764ec7042929
Valerii Vasin <vvasin@groupon.com>
2020-05-24 12:47:32 +0200

    feat: eva run

---
+++ run/eva.ts
@@ -0,0 +1,17 @@
+import { enchant } from '../helpers/enchant';
+import { Item } from '../types';
+
+const enchants: Array<[number, Item]> = [
+  [3, Item.TalismanOfEva_3],
+  [4, Item.TalismanOfEva_4],
+  [5, Item.TalismanOfEva_5],
+  [6, Item.TalismanOfEva_6],
+  [7, Item.TalismanOfEva_7],
+];
+
+for (let [lvl, item] of enchants) {
+  console.log(
+    `Талисман Евы +0 => +${lvl}`,
+    enchant(Item.TalismanOfEva, item, 1000)
+  );
+}
