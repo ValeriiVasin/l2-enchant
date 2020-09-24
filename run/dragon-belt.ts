afc9e4240a0e986d918fa9055ef39c205d6983bb
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-25 01:32:46 +0200

    feat: increase runs

--- run/dragon-belt.ts
+++ run/dragon-belt.ts
@@ -2,17 +2,19 @@ import { enchant } from '../helpers/enchant';
 import { header } from '../helpers/header';
 import { Item } from '../types';
 
+const times = 10_000;
+
 console.log(header('Пояс Дракона +0 => +4'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_4, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_4, times));
 console.log(header('Пояс Дракона +0 => +5'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_5, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_5, times));
 console.log(header('Пояс Дракона +0 => +6'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_6, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_6, times));
 console.log(header('Пояс Дракона +0 => +7'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_7, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_7, times));
 console.log(header('Пояс Дракона +0 => +8'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_8, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_8, times));
 console.log(header('Пояс Дракона +0 => +9'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_9, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_9, times));
 console.log(header('Пояс Дракона +0 => +10'));
-console.log(enchant(Item.DragonBelt, Item.DragonBelt_10, 1000));
+console.log(enchant(Item.DragonBelt, Item.DragonBelt_10, times));
--- run/talisman-of-aden.ts
+++ run/talisman-of-aden.ts
@@ -1,14 +1,22 @@
-import { header } from '../helpers/header';
 import { enchant } from '../helpers/enchant';
+import { header } from '../helpers/header';
 import { Item } from '../types';
 
+const times = 10_000;
+
+console.log(header('Талисман Адена +0 => +1'));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_1, times));
+console.log(header('Талисман Адена +0 => +2'));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_2, times));
+console.log(header('Талисман Адена +0 => +3'));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_3, times));
 console.log(header('Талисман Адена +0 => +4'));
-console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_4, 1000));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_4, times));
 console.log(header('Талисман Адена +0 => +5'));
-console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_5, 1000));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_5, times));
 console.log(header('Талисман Адена +0 => +6'));
-console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_6, 1000));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_6, times));
 console.log(header('Талисман Адена +0 => +7'));
-console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_7, 1000));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_7, times));
 console.log(header('Талисман Адена +0 => +8'));
-console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_8, 1000));
+console.log(enchant(Item.TalismanOfAden, Item.TalismanOfAden_8, times));
