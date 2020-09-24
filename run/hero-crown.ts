4713bb09d47602685da6c779d87d2e4eebd1d950
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-25 01:34:17 +0200

    feat: increase runs for hero-crown

--- run/hero-crown.ts
+++ run/hero-crown.ts
@@ -2,15 +2,17 @@ import { enchant } from '../helpers/enchant';
 import { header } from '../helpers/header';
 import { Item } from '../types';
 
+const times = 10_000;
+
 console.log(header('Венец Героя +0 => +3'));
-console.log(enchant(Item.HeroCrown, Item.HeroCrown_3, 1000));
+console.log(enchant(Item.HeroCrown, Item.HeroCrown_3, times));
 console.log(header('Венец Героя +0 => +4'));
-console.log(enchant(Item.HeroCrown, Item.HeroCrown_4, 1000));
+console.log(enchant(Item.HeroCrown, Item.HeroCrown_4, times));
 console.log(header('Венец Героя +0 => +5'));
-console.log(enchant(Item.HeroCrown, Item.HeroCrown_5, 1000));
+console.log(enchant(Item.HeroCrown, Item.HeroCrown_5, times));
 console.log(header('Венец Героя +0 => +6'));
-console.log(enchant(Item.HeroCrown, Item.HeroCrown_6, 1000));
+console.log(enchant(Item.HeroCrown, Item.HeroCrown_6, times));
 console.log(header('Венец Героя +0 => +7'));
-console.log(enchant(Item.HeroCrown, Item.HeroCrown_7, 1000));
+console.log(enchant(Item.HeroCrown, Item.HeroCrown_7, times));
 console.log(header('Венец Героя +0 => +8'));
-console.log(enchant(Item.HeroCrown, Item.HeroCrown_8, 1000));
+console.log(enchant(Item.HeroCrown, Item.HeroCrown_8, times));
