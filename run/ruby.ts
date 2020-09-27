5c30b95f6595da2bdbdf55f58bdffed8393c83ab
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-27 22:38:34 +0200

    feat: ruby enchants

---
+++ run/ruby.ts
@@ -0,0 +1,8 @@
+import { runWithResolution } from '../helpers/run-with-resolution';
+import { Item } from '../items';
+
+runWithResolution(
+  Item.Ruby_1,
+  [Item.Ruby_2, Item.Ruby_3, Item.Ruby_4, Item.Ruby_5],
+  10_000,
+);
