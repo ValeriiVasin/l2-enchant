2f7cae0e9ffbed8e598de7edcce1646ef8b1eb32
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-27 13:37:24 +0200

    feat: add rune ratio

--- run/rune.ts
+++ run/rune.ts
@@ -19,3 +19,31 @@ const enchants: Array<[number, Item]> = [
 for (const [lvl, item] of enchants) {
   console.log(`Руна Ур.1 => Руна Ур.${lvl}`, enchant(Item.Rune_1, item, 1000));
 }
+printNote();
+
+function printNote() {
+  const count = 1_000_000;
+  const odals = odalCount(count);
+  const ratio = (odals / count).toFixed(2);
+  console.log(
+    `\n* out of ${count} runes you would get ${odals} runes you want. Ratio: ${ratio}`,
+  );
+}
+
+function odalCount(amount: number): number {
+  // Odal in the first cell
+  // total possible runes => 6
+  const runes = [0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < amount; i++) {
+    const index = Math.floor(Math.random() * 6);
+    runes[index]++;
+  }
+
+  const odals = runes[0];
+  // collapse runes back 2 runes = 1 not opened
+  const collapsed = runes
+    .slice(1)
+    .reduce((acc, amount) => acc + Math.floor(amount / 2), 0);
+
+  return collapsed === 0 ? odals : odals + odalCount(collapsed);
+}
