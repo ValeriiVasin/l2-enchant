958d145e5f807e5009fccff324ff8ad966ee101e
Valerii Vasin <valerii.vasin@gmail.com>
2020-09-27 21:56:27 +0200

    refactor: extract resolutions run into helper

--- run/brooch.ts
+++ run/brooch.ts
@@ -1,20 +1,8 @@
-import { enchant } from '../helpers/enchant';
-import { resolveItems } from '../helpers/resolve-items';
+import { runWithResolution } from '../helpers/run-with-resolution';
 import { Item } from '../items';
 
-const enchants: Item[] = [
-  Item.Brooch_2,
-  Item.Brooch_3,
-  Item.Brooch_4,
-  Item.Brooch_5,
-];
-
-let resolutions: Map<Item, Map<Item, number>> = new Map();
-for (const item of enchants) {
-  const enchantResult = enchant(Item.Brooch_1, item, 1000);
-  const resolvedEnchantResult = resolveItems(enchantResult, resolutions);
-
-  resolutions.set(item, resolvedEnchantResult);
-
-  console.log(`${Item.Brooch_1} => ${item}`, resolvedEnchantResult);
-}
+runWithResolution(
+  Item.Brooch_1,
+  [Item.Brooch_2, Item.Brooch_3, Item.Brooch_4, Item.Brooch_5],
+  1000,
+);
