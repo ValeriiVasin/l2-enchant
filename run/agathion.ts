6ae11bdd2cb157c7d898ad7813c4ca2c95649a53
Valerii Vasin <valerii.vasin@gmail.com>
2020-10-05 23:06:42 +0200

    feat: add enchant strategy for high grade agathion

--- run/agathion.ts
+++ run/agathion.ts
@@ -9,8 +9,27 @@ run(
     Item.Agathion_6,
     Item.Agathion_7,
     Item.Agathion_8,
-    Item.Agathion_9,
-    Item.Agathion_10,
+    // Item.Agathion_9,
+    // Item.Agathion_10,
+  ],
+  1000,
+);
+
+run(Item.Agathion_10, [Item.HighQualityAgathion], 1);
+
+run(
+  Item.HighQualityAgathion,
+  [
+    Item.HighQualityAgathion_1,
+    Item.HighQualityAgathion_2,
+    Item.HighQualityAgathion_3,
+    Item.HighQualityAgathion_4,
+    Item.HighQualityAgathion_5,
+    Item.HighQualityAgathion_6,
+    Item.HighQualityAgathion_7,
+    Item.HighQualityAgathion_8,
+    Item.HighQualityAgathion_9,
+    Item.HighQualityAgathion_10,
   ],
   1000,
 );
