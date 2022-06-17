e9654ae8e2f0845484cd8d5a69fb155fb791085a
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-17 23:59:38 +0200

    feat: add runs for talisman of insolence

---
+++ run/talisman-of-insolence.ts
@@ -0,0 +1,14 @@
+import { run } from '../helpers/run';
+import { Item } from '../items';
+
+run(
+  Item.TalismanOfInsolence_1,
+  [
+    Item.TalismanOfInsolence_2,
+    Item.TalismanOfInsolence_3,
+    Item.TalismanOfInsolence_4,
+    Item.TalismanOfInsolence_5,
+    Item.TalismanOfInsolence_6,
+  ],
+  10_000,
+);
