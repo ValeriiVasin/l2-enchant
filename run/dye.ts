6c2a932b5d56ce522be3d35543ef6643e2f17ec7
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-24 20:50:19 +0200

    feat: dyes (#165)
    * feat: enchant dye
    
    * feat: higher level of dye

---
+++ run/dye.ts
@@ -0,0 +1,30 @@
+import { run } from '../helpers/run';
+import { Item } from '../items';
+
+run(
+  Item.Dye,
+  [
+    Item.Dye_1,
+    Item.Dye_2,
+    Item.Dye_3,
+    Item.Dye_4,
+    Item.Dye_5,
+    Item.Dye_6,
+    Item.Dye_7,
+    Item.Dye_8,
+    Item.Dye_9,
+    Item.Dye_10,
+    Item.Dye_11,
+
+    Item.Dye_12,
+    Item.Dye_13,
+    Item.Dye_14,
+    Item.Dye_15,
+    Item.Dye_16,
+    Item.Dye_17,
+    Item.Dye_18,
+    Item.Dye_19,
+    Item.Dye_20,
+  ],
+  100_000,
+);
