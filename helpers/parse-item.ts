da21734689767e98fb71a3c18ddf32975fb75df9
Valerii Vasin <valerii.vasin@gmail.com>
2022-06-25 15:23:54 +0200

    feat: parse item

---
+++ helpers/parse-item.ts
@@ -0,0 +1,19 @@
+const levelRegexp = /Ур.(\d+)/;
+const parseLevel = (item: string): number => {
+  const match = item.match(levelRegexp);
+  return match ? Number(match[1]) : 0;
+};
+
+const enchantRegexp = /\+(\d+)$/;
+const parseEnchant = (item: string): number => {
+  const match = item.match(enchantRegexp);
+  return match ? Number(match[1]) : 0;
+};
+
+const parseBase = (item: string): string => {
+  return item.replace(levelRegexp, '').replace(enchantRegexp, '').trim();
+};
+
+export function parseItem(item: string): { base: string; level: number; enchant: number } {
+  return { base: parseBase(item), level: parseLevel(item), enchant: parseEnchant(item) };
+}
