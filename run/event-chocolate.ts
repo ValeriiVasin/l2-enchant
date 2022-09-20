b1b20c7ac2cdba536e745ad1cd3f6355b5806709
Valerii Vasin <valerii.vasin@gmail.com>
2022-09-21 01:30:19 +0200

    feat: event chocolate run (#226)
    * feat: add chocolate run
    
    * feat: enchants to 17
    
    * fix: unit tests

--- run/event-chocolate.ts
+++ run/event-chocolate.ts
@@ -2,4 +2,4 @@ import { itemsRange } from '../helpers/items-range';
 import { run } from '../helpers/run';
 import { Item } from '../items';
 
-run(Item.EventDragonSlayerSword, itemsRange(Item.EventDragonSlayerSword_7, Item.EventDragonSlayerSword_17), 100_000);
+run(Item.ChocolateFigure_1, itemsRange(Item.ChocolateFigure_2, Item.ChocolateFigure_15), 100_000);
