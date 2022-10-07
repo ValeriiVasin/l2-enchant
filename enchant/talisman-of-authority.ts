fd15b031124f2aa08c4c7d89bd5d6daa8a40357a
Valerii Vasin <valerii.vasin@gmail.com>
2022-10-07 17:10:51 +0200

    fix: adjust authority chances (#234)

--- enchant/talisman-of-authority.ts
+++ enchant/talisman-of-authority.ts
@@ -6,7 +6,7 @@ import { EnchantStrategy } from '../types';
 export const talismanOfAuthorityEnchantMap = createEnchantMap({
   strategy: EnchantStrategy.Drop,
   enchantItem: Item.TalismanOfAuthorityFragment,
-  chances: [70, 60, 50, 40, 50, 60, 50, 40, 30, 50, 30, 25, 20, 10, 50, 25, 20, 15, 10],
+  chances: [70, 60, 50, 40, 50, 60, 50, 40, 30, 50, 40, 30, 20, 10, 50, 25, 20, 15, 10],
   items: itemsRange(Item.TalismanOfAuthority_1, Item.TalismanOfAuthority_20),
   boundaries: new Set([Item.TalismanOfAuthority_6, Item.TalismanOfAuthority_11, Item.TalismanOfAuthority_16]),
 });
